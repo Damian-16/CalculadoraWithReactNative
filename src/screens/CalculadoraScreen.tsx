79f04ff0684e727da4513d77e39e1345852166c2
Damian Pinedo <DamianMorganti16@hotmail.com>
2021-05-31 12:05:10 -0400

    se agrego el primero boton de la calculadora

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -4,8 +4,14 @@ import { styles } from '../styles/CalculadoraScreenStyle';
 
 export const CalculadoraScreen = () => {
     return (
-        <View>
-            <Text style={styles.text}>Calculadora Screen</Text>
+        <View style={styles.calculadoraContainer}>
+             <Text style={styles.littleResult}>1.500</Text>
+            <Text style={styles.result}>1.500</Text>
+
+            <View style={styles.button}>
+                {/*Boton */}
+                <Text style={styles.buttonText}>1</Text>
+            </View>
         </View>
     )
 }
--- src/styles/CalculadoraScreenStyle.tsx
+++ src/styles/CalculadoraScreenStyle.tsx
@@ -4,10 +4,40 @@ export  const styles = StyleSheet.create({
     theme:{
         flex:1,
         backgroundColor:'black',
+       
     },
-    text:{
+    calculadoraContainer:{
+        paddingHorizontal:20,
+        flex:1,
+        justifyContent:"flex-end",
+    },
+
+   result:{
         color:"white",
-        fontSize:20,
-    }
+        fontSize:60,
+        textAlign:'right',
+
+    },
+    littleResult:{
+        color:'rgba(255,255,255,0.5)',
+        fontSize:30,
+        textAlign:'right'
+
+    },
+    button:{
+        height:40,
+        width:40,
+        backgroundColor:'#333333',
+        borderRadius:100,
+        justifyContent:"center",
+    },
+    buttonText:{
+    textAlign:'center',
+    padding:5,
+    fontSize:20,
+    color:'white',
+    fontWeight:'bold',
+    },
+
 
 })
